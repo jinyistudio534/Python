5feb28921ce68db71d7f2fb93a6e1737f5cd4208
jinyistudio <121013147+jinyistudio534@users.noreply.github.com>
2024-11-25 11:22:44 +0800

    Update main.py

--- Flet/circleimage/main.py
+++ Flet/circleimage/main.py
@@ -1,1 +1,25 @@
+import flet as ft
 
+def main(page: ft.Page):    
+    # Define page related settings
+    #page.theme_mode = ft.ThemeMode.DARK
+
+    body=ft.Container(
+        bgcolor="white10",
+        margin=ft.margin.all(30),
+        width=200,
+        height=200,
+        shape=ft.BoxShape("circle"),
+        # Define image for profile picture
+        image_src="/EasyCAT shield.png",
+        image_fit="cover",
+        shadow=ft.BoxShadow(
+            spread_radius=6,
+            blur_radius=20,
+            color=ft.colors.with_opacity(0.71, "grey"),
+        ),
+    )
+    page.add(body)
+
+
+ft.app(target=main, assets_dir="assets")
